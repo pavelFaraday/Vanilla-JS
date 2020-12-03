3e34ab2bab7202b934e31842c89e499e8388e29d
pavelFaradey <devstudio.corp@gmail.com>
2020-12-03 17:25:37 +0400

    Objects and Prototypes/ create object

---
+++ Objects and Prototypes/Createing Objects/script.js
@@ -0,0 +1,34 @@
+// 1) create object:
+let emp1= {};
+emp1.name = 'John';
+emp1.surname = 'Falcone';
+emp1.gender = 'female';
+emp1.designation = 'Regional Manager';
+console.log(emp1);
+
+/* -------------------------------------------------------------------------- */
+
+// 2) create object with function:
+function user(name, surname, gender) {
+    let newUser = {};
+    newUser.name = name;
+    newUser.surname = surname;
+    newUser.gender = gender;
+    return newUser;
+}
+
+let myUser = user('gorg', 'borxes', 'male');
+console.log(myUser);
+
+/* -------------------------------------------------------------------------- */
+
+// 3) create object with Constructor function:
+function Bysicle(speed, brand, weight) {
+    this.speed = speed;
+    this.brand = brand;
+    this.weight = weight;
+}
+
+myBysicle = new Bysicle(120, 'Toyota', 59);
+console.log(myBysicle);
+
