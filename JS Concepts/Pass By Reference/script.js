51184251538718daf9fbb648ca3a27a71726c715
pavelFaradey <devstudio.corp@gmail.com>
2020-12-05 14:57:32 +0400

    Pass By Reference

--- JS Concepts/Pass By Reference/script.js
+++ JS Concepts/Pass By Reference/script.js
@@ -5,24 +5,60 @@
 /* When you pass a variable to a function, the function is not creating a copy of what you passed. It is keeping a reference back to the original thing that you passed. 
 If you make changes to the local variable in the function then you are really making the changes to the original thing that you sent to the function. */
 
+
+/* ------------------------------- With Arrays ------------------------------ */
+
 /* 
-there are 2 ways to copy an array: 
+There are 2 ways to copy an original array: 
 1) Array.from(arr);
 2) [].concat(arr);
 */
 
-
 let names = ['Bob', 'Louise', 'Tina', 'Linda', 'Gene'];
 
 let f = function(arr){
-    let a = Array.from(arr);
-    let r = [].concat(arr);
-    arr.push('Mort');
-    a.push('Belcher');
+    let a = Array.from(arr); // make/copy brand new array
+    let r = [].concat(arr); // make/copy brand new array
+    arr.push('Mort'); // push in original arrays - Affects original arrays
+    a.push('Belcher'); // it doesn't affects original array, because a[] is a copy of names[]
     console.log(arr);
     console.log(names);
     console.log(a);
     console.log(r);
 };
 
-f(names);
+f(names); // names[] is original array and is affecting with .push() in function
+
+
+/* ------------------------------ With Objects ------------------------------ */
+
+/* 
+   How to copy an original object: 
+   let o = Object.assign({}, obj);
+*/
+
+let archer = {
+    characters: ['Sterling', 'Pam', 'Lana', 'Mallory'],
+    info:{inner:true, nums:[1,2,3]}
+    };
+
+let g = (obj) => {
+    let o = Object.assign({}, obj);
+    obj.characters = ['Slater', 'Sterling', 'Lana'];
+    obj.characters.push('Cheryl');
+    obj.info.inner = false;
+    obj.info.nums = [4,5,6];
+    
+    //o.info = Object.assign({}, obj.info);
+    
+    log('OBJ', obj);
+    log('ARCHER', archer);
+    log('O', o);
+    return o;
+};
+
+let newObj = g(archer);
+newObj.characters.push('Cyril');
+newObj.info.nums.push(8);
+log('NEWOBJ', newObj);
+log('ARCHER', archer);
