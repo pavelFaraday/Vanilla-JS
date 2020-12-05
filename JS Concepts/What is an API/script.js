f7da51f7c2723753d04324558b5d83809d8d9e6d
pavelFaradey <devstudio.corp@gmail.com>
2020-12-05 16:51:58 +0400

    What is an API?

---
+++ JS Concepts/What is an API/script.js
@@ -0,0 +1,29 @@
+/* -------------------------------------------------------------------------- */
+/*                               What is an API?                              */
+/* -------------------------------------------------------------------------- */
+
+/*  
+API - Application Programming Interface.
+
+API is a software intermediary that allows two applications to talk to each other. Each time you use an app like Facebook, send an instant message, or check the weather on your phone, you're using an API.
+
+To speak plainly, it's a messenger, that takes your requests and tells the system what do you want and then returns response back to you. So, whenever you think of an API, just think of it as your waiter running back and forth between applications, databases and devices to deliver data and create connectivity.
+https://www.youtube.com/watch?v=s7wmiS2mSXY
+
+---------------------------
+
+When you use an application on your mobile phone, the application connects to the Internet and sends data to a server. The server then retrieves that data, interprets it, performs the necessary actions and sends it back to your phone. The application then interprets that data and presents you with the information you wanted in a readable way. This is what an API is - all of this happens via API.
+
+Imagine you’re sitting at a table in a restaurant with a menu of choices to order from. The kitchen is the part of the “system” that will prepare your order. What is missing is the critical link to communicate your order to the kitchen and deliver your food back to your table. That’s where the waiter or API comes in. The waiter is the messenger – or API – that takes your request or order and tells the kitchen – the system – what to do. Then the waiter delivers the response back to you; in this case, it is the food.
+
+Here is a real-life API example. You may be familiar with the process of searching flights online. Just like the restaurant, you have a variety of options to choose from, including different cities, departure and return dates, and more. Let us imagine that you’re booking you are flight on an airline website. You choose a departure city and date, a return city and date, cabin class, as well as other variables. In order to book your flight, you interact with the airline’s website to access their database and see if any seats are available on those dates and what the costs might be.
+
+However, what if you are not using the airline’s website––a channel that has direct access to the information? What if you are using an online travel service, such as Kayak or Expedia, which aggregates information from a number of airline databases?
+
+The travel service, in this case, interacts with the airline’s API. The API is the interface that, like your helpful waiter, can be asked by that online travel service to get information from the airline’s database to book seats, baggage options, etc. The API then takes the airline’s response to your request and delivers it right back to the online travel service, which then shows you the most updated, relevant information.
+
+---------------------------
+
+An API is some code, that you didn't write yourself and that code has some methods that you are allowed to call on the code. This code somebody else is written that I have access to that I can use in my own script to accomplish some sort of task. The code could be something that you download and you run from your own computer or it could be something that's someplace on the WEB. An API can be sitting on a remote server, it can be sitting on my own computer, it could be something that I have to use HTTP to talk to. I have to make AJAX requests to the API to get information back.
+
+*/
