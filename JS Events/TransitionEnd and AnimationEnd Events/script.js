ea407f92480124b9569382b1ec5089ef10d1bdae
pavelFaradey <devstudio.corp@gmail.com>
2020-12-13 18:04:02 +0400

    Transition &; Animation Events

---
+++ JS Events/TransitionEnd and AnimationEnd Events/script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function(){
+    let ps = document.querySelectorAll('p');
+    ps.forEach(p=>{
+        p.addEventListener('transitionend', glow);
+        p.addEventListener('animationend', noglow);
+    });
+});
+
+function glow(ev){
+    console.log('transitionend');
+    ev.currentTarget.style.animationName = 'glow';
+}
+
+function noglow(ev){
+    ev.currentTarget.style.animationName = '';
+}
