42e24c74cdba0e5097afc97152ea21037ad52b0a
pavelFaraday <devstudio.corp@gmail.com>
2021-09-24 20:25:54 +0400

    Vanilla JS - Modern JS Nuggets | Array.form method

--- Modern JS Nuggets/11-array-from/app.js
+++ Modern JS Nuggets/11-array-from/app.js
@@ -1,18 +1,20 @@
 // Array.from - NOT ON PROTOTYPE
-
 // from - returns Array Object from an object
 // with a length property or an iterable object
-// from - turns array-like/ish into array - string, nodeList, Set
+// from - turns array-like/ish into array - string, nodeList, Set !!!  Set/nodeList/string -> array
+// https://youtu.be/zg1Bv4xubwo?list=PLnHJACx3NwAfRUcuKaYhZ6T5NRIpzgNGJ
+
 
 const udemy = 'udemy'
-
-// console.log(Array.from(udemy))
+console.log(Array.from(udemy)) // ['u', 'd', 'e', 'm', 'y']
 
 const text = document.querySelectorAll('.text')
 // console.log(text)
 
 const newText = Array.from(text).find((item) => item.textContent === 'person')
 // console.log(newText)
+
+/* --------------- Create Pagination with Array.from() method --------------- */
 
 const items = Array.from({ length: 120 }, (_, index) => {
   return index
